dd2d7ecfc83c89e0db60be5d0ed235ce89c892aa
Mark Thomas <mark.highland@gmail.com>
2023-01-11 17:23:22 +0000

    Daily commit

--- entryform.py
+++ entryform.py
@@ -23,6 +23,8 @@ from flet import (
     Column,
     TextThemeStyle,
     UserControl,
+    InputBorder,
+    TextCapitalization,
 )
 from flet.event import Event
 import model
@@ -39,44 +41,47 @@ class Entry(UserControl):
 
     def build(self):
         self.entry_row = Row(
+            width=700,
             alignment="spaceBetween",
             vertical_alignment="center",
             controls=[
                 Text(self.entry, width=70),
                 Text(self.description, width=500),
-                Text(self.count, width=80),
-                Row(
-                    alignment="center",
-                    controls=[
-                        IconButton(
-                            icon=icons.EDIT_OUTLINED,
-                            tooltip="Return entry for editing",
-                            on_click=self.edit_clicked,
-                        ),
-                        IconButton(
-                            icons.DELETE_OUTLINE,
-                            tooltip="Delete entry",
-                            on_click=self.delete_clicked,
-                        ),
-                    ],
+                TextField(
+                    value=self.count,
+                    width=80,
+                    border=InputBorder.NONE,
+                    on_submit=self.switch_count,
+                ),
+                IconButton(
+                    icons.DELETE_OUTLINE,
+                    tooltip="Delete entry",
+                    on_click=self.delete_clicked,
                 ),
             ],
         )
         return Column(controls=[self.entry_row])
 
-    def edit_clicked(self, e):
-        # TODO copy data back and delete row
-        self.update()
-
     def delete_clicked(self, e):
         delete_entry(self)
+
+    def switch_count(self, event):
+        if event.control.value not in ("1", "2"):
+            event.control.value = "1"
+        self.count = event.control.value
+        tally_count()
+        event.page.update()
+        if _debug:
+            print("switch_count")
+            print(event)
+            sys.stdout.flush()
 
 
 def check_existing_exibitor(event: Event) -> None:
     name = exhibitor_name.value
     event.page.update()
     if _debug:
-        print("entryform_support.check_existing_exibitor")
+        print("check_existing_exibitor")
         print(name)
         sys.stdout.flush()
     values = model.exhibitor_check(name)
@@ -87,7 +92,7 @@ def check_existing_exibitor(event: Event) -> None:
 
 def clear_all(event):
     if _debug:
-        print("entryform_support.clear_all")
+        print("clear_all")
         print(event)
         sys.stdout.flush()
 
@@ -96,68 +101,51 @@ def create_entry(event):
     if not description.value or description.value == "Class Description":
         set_description(event)
     if description.value.startswith("No such"):
-        return
+        return None
     entries.controls.append(
         Entry(display_class.value, description.value, count.value)
     )
     display_class.value = ""
     description.value = ""
-    count.value = 1
+    count.value = "1"
+    tally_count()
+    event.page.update()
+    display_class.focus()
+    if _debug:
+        print("create_entry")
+        print(event)
+        sys.stdout.flush()
+
+
+def tally_count():
     sum = 0
     for entry in entries.controls:
         sum += int(entry.count)
     entry_count.value = f"Total entries {sum}"
-    event.page.update()
-    if _debug:
-        print("entryform_support.create_entry")
-        print(event)
-        sys.stdout.flush()
-
-
-def create_exhibitor(event):
-    if _debug:
-        print("entryform_support.create_exhibitor")
-        print(event)
-        sys.stdout.flush()
 
 
 def delete_entry(entry):
     if _debug:
-        print("entryform_support.delete_entry")
-        sys.stdout.flush()
-
-
-def entry_selected(event):
-    if _debug:
-        print("entryform_support.entry_selected")
-        print(event)
+        print("delete_entry")
         sys.stdout.flush()
 
 
 def post_to_model(event):
     if _debug:
-        print("entryform_support.post_to_model")
-        print(event)
-        sys.stdout.flush()
-
-
-def replace_current_entry(event):
-    if _debug:
-        print("entryform_support.replace_current_entry")
+        print("post_to_model")
         print(event)
         sys.stdout.flush()
 
 
 def set_description(event):
-    class_entered = display_class.value.upper()
-    display_class.value = class_entered
+    class_entered = display_class.value
     description.value = model.get_class_description(class_entered)
     if description.value.startswith("No such"):
         display_class.value = ""
         display_class.focus()
     event.page.update()
     if _debug:
-        print("entryform_support.set_description")
+        print("set_description")
         print(event)
         sys.stdout.flush()
 
@@ -171,15 +159,17 @@ member = Checkbox(label="Member?", label_position="right")
 add = ElevatedButton("Add", icon=icons.ADD, on_click=create_entry)
 # second line
 display_class = TextField(
-    label="Class", value="", width=70, on_submit=set_description
+    label="Class",
+    value="",
+    width=70,
+    capitalization=TextCapitalization.WORDS,
+    on_submit=set_description,
 )
 description = Text("Class Description", width=500, size=20)
 count = Dropdown(
     value="1",
     options=[dropdown.Option("1"), dropdown.Option("2")],
     width=80,
-    on_focus=set_description,
-    on_change=create_entry
 )
 # accumulated entries
 entries = Column()
