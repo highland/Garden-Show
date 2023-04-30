3fd4bdb8e65063667ea15d24866ba1796b331fd6
Mark Thomas <mark.highland@gmail.com>
2023-04-30 19:41:18 +0100

    Tidying up indents

--- garden_show/entryform.py
+++ garden_show/entryform.py
@@ -1,3 +1,5 @@
+
+
 # -*- coding: utf-8 -*-
 """
 GUI supporting recording and editing the Entry Forms
--- garden_show/reporter.py
+++ garden_show/reporter.py
@@ -23,30 +23,41 @@ class Output(ft.ListView):
     def __init__(self, *kwargs):
         super().__init__(*kwargs)
         self.auto_scroll = True
+        self.height = 600
 
     def print(
         self, text: str, indent: int = 0, style: Style = Style.BODY
     ) -> None:
-        self.controls.append(ft.Text(" " * indent + text, style=style.value))
+        self.controls.append(
+            ft.Text(
+                text,
+                style=style.value,
+                offset=ft.transform.Offset(0.02 * indent, 0),
+            )
+        )
 
     def clear(self) -> None:
         self.controls = []
+
+    def divider(self) -> None:
+        self.controls.append(ft.Divider())
 
 
 def main(page: ft.Page):
     def show_results(event: ft.ControlEvent) -> None:
         output.clear()
         output.print("Results by Exhibitor", style=Style.TITLE)
+        output.divider()
         awards = Show.awards.get_all_awards()
         for exhibitor in Show.exhibitors:
             if exhibitor.results:
                 output.print(
-                    f"Exhibitor {exhibitor}", style=Style.HEADING, indent=4
+                    f"Exhibitor {exhibitor}", style=Style.HEADING, indent=1
                 )
                 for result in exhibitor.results:
                     output.print(
                         f"{result.place.value}{result.show_class.class_id}",
-                        indent=8,
+                        indent=2,
                     )
 
             for award in awards:
@@ -55,21 +66,21 @@ def main(page: ft.Page):
                         output.print(
                             f"Winner of {award.name}:",
                             style=Style.BIGBODY,
-                            indent=8,
+                            indent=2,
                         )
 
                         output.print(
                             f"{award.description} "
                             f"{f'for {award.reason}' if award.reason else ''}",
                             style=Style.BODY,
-                            indent=12,
+                            indent=4,
                         )
 
                     elif award.wins is Show.awards.WinsType.ROSETTE:
                         output.print(
-                            f"        "
                             f"Awarded a Rosette for {award.description}",
                             style=ft.TextThemeStyle.BODY_LARGE,
+                            indent=2,
                         )
 
         event.page.update()
